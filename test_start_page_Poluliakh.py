b828811bbcc1dc1d1e7df9c725958eeca49338c0
maksym.po <maksym.po@room4.team>
2022-09-17 21:13:03 +0300

    Added 3 test: test_signin_empty,

--- test_start_page_Poluliakh.py
+++ test_start_page_Poluliakh.py
@@ -21,10 +21,12 @@ class TestStartPage:
         driver.get('https://qa-complex-app-for-testing.herokuapp.com/')
 
         sleep(1)
+
         # Fill username
+        user = random()
         username = driver.find_element(by=By.XPATH, value='.//*[@id="username-register"]')
         username.clear()
-        username.send_keys(random())
+        username.send_keys(user)
         sleep(1)
 
         # Fill email
@@ -46,6 +48,7 @@ class TestStartPage:
         # Verify sign up successful
         verify_element = driver.find_element(by=By.XPATH, value='.//*/button[contains(text(),"Sign Out")]')
         assert verify_element.text == "Sign Out"
+        assert driver.find_element(by=By.XPATH, value=".//strong").text == user.lower()
 
 
         # Close driver
@@ -85,7 +88,7 @@ class TestStartPage:
         verify_element = driver.find_element(by=By.XPATH,
                                              value='.//*/div[contains(text(),"Username must be at least 3 characters")]')
         assert verify_element.text == "Username must be at least 3 characters."
-        assert driver.find_element(by=By.XPATH, value=".//strong").text == username
+
         driver.close()
 
     def test_empty_email(self):
@@ -119,3 +122,93 @@ class TestStartPage:
         assert verify_element.text == "You must provide a valid email address."
 
         driver.close()
+
+    def test_empty_password(self):
+        driver = webdriver.Chrome(r"C:\Users\HomeWork\PycharmProjects\pythonProject\chromedriver.exe")
+
+        # Open page
+        driver.get('https://qa-complex-app-for-testing.herokuapp.com/')
+
+        sleep(1)
+
+        username = driver.find_element(by=By.XPATH, value='.//*[@id="username-register"]')
+        username.clear()
+        username.send_keys(random())
+        sleep(1)
+
+        email = driver.find_element(by=By.XPATH, value='.//*[@id="email-register"]')
+        email.clear()
+        email.send_keys(f"{random()}@gmail.com")
+        sleep(1)
+
+        password = driver.find_element(by=By.XPATH, value='.//*[@id="password-register"]')
+        password.clear()
+        sleep(1)
+
+        button = driver.find_element(by=By.XPATH, value='.//*[@type="submit"]')
+        button.click()
+        sleep(1)
+
+        # Verify sign up successful
+        verify_element = driver.find_element(by=By.XPATH,
+                                             value='.//*/div[contains(text(),"Password must be at least 12 characters.")]')
+        assert verify_element.text == "Password must be at least 12 characters."
+
+        driver.close()
+
+    def test_signin_successful(self):
+        # Create driver
+        driver = webdriver.Chrome(r"C:\Users\HomeWork\PycharmProjects\pythonProject\chromedriver.exe")
+
+        # Open page
+        driver.get("https://qa-complex-app-for-testing.herokuapp.com/")
+
+        # Fill login
+        login = driver.find_element(by=By.XPATH, value=".//input[@placeholder='Username']")
+        login.clear()
+        login.send_keys("MaxP")
+        sleep(1)
+
+        # Fill password
+        password = driver.find_element(by=By.XPATH, value=".//input[@placeholder='Password']")
+        password.clear()
+        password.send_keys("Test7777test")
+        sleep(1)
+
+        # Click button
+        button = driver.find_element(by=By.XPATH, value=".//button[text()='Sign In']")
+        button.click()
+        sleep(1)
+
+        # Verify sign in
+        hello_message = driver.find_element(by=By.XPATH, value=".//h2")
+        assert hello_message.text == f"Hello {'MaxP'.lower()}, your feed is empty."
+
+        # Close driver
+        driver.close()
+
+    def test_signin_empty(self):
+        # Create driver
+        driver = webdriver.Chrome(r"C:\Users\HomeWork\PycharmProjects\pythonProject\chromedriver.exe")
+
+        # Open page
+        driver.get("https://qa-complex-app-for-testing.herokuapp.com/")
+
+        # Empty login
+        login = driver.find_element(by=By.XPATH, value=".//input[@placeholder='Username']")
+        login.clear()
+        sleep(1)
+
+        # Empty password
+        password = driver.find_element(by=By.XPATH, value=".//input[@placeholder='Password']")
+        password.clear()
+        sleep(1)
+
+        # Click button
+        button = driver.find_element(by=By.XPATH, value=".//button[text()='Sign In']")
+        button.click()
+        sleep(1)
+
+        # Verify error
+        error_element = driver.find_element(by=By.XPATH, value=".//div[@class='alert alert-danger text-center']")
+        assert error_element.text == "Invalid username / pasword", f"Actual message: {error_element.text}"
